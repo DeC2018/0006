b0e439d29cdcafe2ceaffabc0feb3ce529e94abb
Den <dec2018@noreply.gitverse.ru>
2024-11-13 13:36:13 +0000

    create main.cpp

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  string convert(string s, int numRows) {
+    string ans;
+    vector<vector<char>> rows(numRows);
+    int k = 0;
+    int direction = (numRows == 1) - 1;
+
+    for (const char c : s) {
+      rows[k].push_back(c);
+      if (k == 0 || k == numRows - 1)
+        direction *= -1;
+      k += direction;
+    }
+
+    for (const vector<char>& row : rows)
+      for (const char c : row)
+        ans += c;
+
+    return ans;
+  }
+};
+
+int main() {
+  Solution solution;
+  string s = "PAYPALISHIRING";
+  int numRows = 3;
+  string result = solution.convert(s, numRows);
+  cout << "Result: " << result << endl;
+  return 0;
+}
+
+
